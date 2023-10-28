1b5413203aaf619f8279af9ccec9a8e325b8dbf8
Sujan Sapkota <sujan.sapkota@student.hogent.be>
2023-10-28 20:14:56 +0200

    minor change

--- src/service/user.js
+++ src/service/user.js
@@ -1,6 +1,5 @@
 const userRepository = require('../repository/user');
 
-
 const getAll = async () => {
     const data  = await userRepository.getAll();
     return { items: data, count: data.length };
